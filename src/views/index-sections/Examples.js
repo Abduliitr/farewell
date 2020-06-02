acf1688322bcefaa6158365350e8b8c353ba8af9
Abduliitr <abdul.thomso@gmail.com>
2020-06-02 22:47:49 +0530

    fixed imp links

--- src/views/index-sections/Examples.js
+++ src/views/index-sections/Examples.js
@@ -13,39 +13,39 @@ function Examples() {
         <Container className="text-center">
           <Row>
             <div className="col">
-              <a href="https://www.iitr.ac.in/" target="_blank">
+              <a href="https://www.facebook.com/CRY.Roorkee/" target="_blank">
                 <img
                   alt="..."
                   className="img-raised"
-                  src={require("assets/img/landing-iitr.png")}
+                  src={require("assets/img/fbPage.png")}
                 ></img>
               </a>
               <Button
                 className="btn-round"
                 color="default"
-                to="https://www.iitr.ac.in/"
                 outline
-                tag={Link}
-              >
-                View IITR Website
+                // tag={Link}
+              ><a href="https://www.facebook.com/CRY.Roorkee/" target="_blank">
+                View FB Page</a>
               </Button>
             </div>
             <div className="col">
-              <a href="https://www.iitr.ac.in/" target="_blank">
+              <Link to="/team-page" target="_blank">
                 <img
                   alt="..."
                   className="img-raised"
-                  src={require("assets/img/landing-iitr.png")}
+                  src={require("assets/img/teamPage.png")}
                 ></img>
-              </a>
+              </Link>
               <Button
                 className="btn-round"
                 color="default"
-                to="https://www.iitr.ac.in/"
+                // to="/team-page"
+                // target="_blank"
                 outline
-                tag={Link}
-              >
-                View Profile Page
+                // tag={Link}
+              ><a href="/team-page" target="_blank">
+                View Team Page</a>
               </Button>
             </div>
           </Row>
